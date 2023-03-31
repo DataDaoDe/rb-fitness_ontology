3084b81a9f0b63d2ff4c524522b1bdb3214b8b92
John Faucett <jsfaucett87@icloud.com>
2023-03-31 15:33:37 +0200

    Add basic access to exercises

--- lib/fitness_ontology/repos/exercise_database.rb
+++ lib/fitness_ontology/repos/exercise_database.rb
@@ -1,6 +1,15 @@
 module FitnessOntology
   module Repos
     class ExerciseDatabase < Base
+      def prefix
+        @prefix ||= %(
+          PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+          PREFIX owl: <http://www.w3.org/2002/07/owl#>
+          PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+          PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>
+          PREFIX edb: <http://www.semanticweb.org/johnfaucett/ontologies/2023/2/exercise-database#>
+        )
+      end
 
       def connect
         @repo = RDF::Repository.load(
@@ -8,6 +17,25 @@ module FitnessOntology
         )
       end
 
+      def query(query)
+        prefixed_query = [prefix, query].join("\n")
+        q = SPARQL.parse(prefixed_query)
+        repo.query(q)
+      end
+
+      def strength_exercises
+        query("SELECT * WHERE { ?subject rdf:type edb:StrengthExercise }").map do |result|
+          result[:subject].fragment
+        end
+      end
+
+      def labels_for(name)
+        results = {}
+        query("SELECT * WHERE { edb:#{name} rdfs:label ?label }").map do |x|
+          results[x[:label].language] = x[:label].value
+        end
+        results
+      end
     end
   end
 end
